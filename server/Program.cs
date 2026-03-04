7067e49b64135de22688e8fa08ebbcd478709209
Håvard Hetland Vestbø <haavarhv@stud.ntnu.no>
2026-03-04 12:08:22 +0100

    Dashboard

--- server/Program.cs
+++ server/Program.cs
@@ -1,16 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
